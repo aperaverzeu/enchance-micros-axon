0c8477116f1d91dd7033184024408008827c08bc
aperaverzeu <dev.mrojaczy@gmail.com>
2022-07-09 17:09:28 +0300

    update Products module for Eureka Discovery

--- ProductsService/src/main/java/eu/aperaverzeu/productsservice/ProductsServiceApplication.java
+++ ProductsService/src/main/java/eu/aperaverzeu/productsservice/ProductsServiceApplication.java
@@ -2,8 +2,10 @@ package eu.aperaverzeu.productsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProductsServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProductsServiceApplication.class, args);
