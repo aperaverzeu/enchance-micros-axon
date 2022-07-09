93d9bad17ab45e549eedf049c7f5eb28ad1518cc
aperaverzeu <dev.mrojaczy@gmail.com>
2022-07-09 16:49:03 +0300

    add Product service module

---
+++ ProductsService/src/main/java/eu/aperaverzeu/productsservice/controller/ProductsController.java
@@ -0,0 +1,29 @@
+package eu.aperaverzeu.productsservice.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/products")
+public class ProductsController {
+
+    @PostMapping
+    public ResponseEntity<String> createProduct() {
+        return ResponseEntity.ok().body("HTTP POST Handled");
+    }
+
+    @GetMapping
+    public ResponseEntity<String> getProduct() {
+        return ResponseEntity.ok().body("Get!");
+    }
+
+    @PutMapping
+    public ResponseEntity<String> updateProduct() {
+        return ResponseEntity.ok().body("Put!");
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteProduct() {
+        return ResponseEntity.ok().body("Delete!");
+    }
+}
