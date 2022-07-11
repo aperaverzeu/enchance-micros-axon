f09370abcece961f627fe71325abb22d0765c6ba
aperaverzeu <dev.mrojaczy@gmail.com>
2022-07-11 21:54:21 +0300

    add message interception

--- ProductsService/src/main/java/eu/aperaverz/productsservice/ProductsServiceApplication.java
+++ ProductsService/src/main/java/eu/aperaverz/productsservice/ProductsServiceApplication.java
@@ -1,13 +1,23 @@
 package eu.aperaverz.productsservice;
 
+import eu.aperaverz.productsservice.command.interceptors.CreateProductCommandInterceptor;
+import org.axonframework.commandhandling.CommandBus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ProductsServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProductsServiceApplication.class, args);
+	}
+
+	@Autowired
+	public void registerCreateProductCommandInterceptor(ApplicationContext applicationContext,
+														CommandBus commandBus) {
+		commandBus.registerDispatchInterceptor(applicationContext.getBean(CreateProductCommandInterceptor.class));
 	}
 }
---
+++ ProductsService/src/main/java/eu/aperaverz/productsservice/command/interceptors/CreateProductCommandInterceptor.java
@@ -0,0 +1,40 @@
+package eu.aperaverz.productsservice.command.interceptors;
+
+import eu.aperaverz.productsservice.command.CreateProductCommand;
+import lombok.extern.slf4j.Slf4j;
+import org.axonframework.commandhandling.CommandMessage;
+import org.axonframework.messaging.MessageDispatchInterceptor;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.function.BiFunction;
+
+@Component
+@Slf4j
+public class CreateProductCommandInterceptor implements MessageDispatchInterceptor<CommandMessage<?>> {
+
+    @Override
+    public BiFunction<Integer, CommandMessage<?>, CommandMessage<?>> handle(List<? extends CommandMessage<?>> list) {
+        return (index, command) -> {
+
+            log.info("Intercepted command: " + command.getPayloadType());
+
+            if (CreateProductCommand.class.equals(command.getPayloadType())) {
+                CreateProductCommand createProductCommand = (CreateProductCommand) command.getPayload();
+
+                if (createProductCommand.getPrice().compareTo(BigDecimal.ZERO) <= 0) {
+                    throw new IllegalArgumentException("Price cannot be zero!");
+                }
+
+                if (createProductCommand.getTitle() == null ||
+                        createProductCommand.getTitle().isEmpty() ||
+                        createProductCommand.getTitle().isBlank()) {
+                    throw new IllegalArgumentException("Title cannot be empty!");
+                }
+            }
+
+            return command;
+        };
+    }
+}
